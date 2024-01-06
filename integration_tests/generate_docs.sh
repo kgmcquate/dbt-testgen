e5d9ac35db6f2ab3be203afb547e62b9b462dc7b
kevin <you@example.com>
2024-01-05 19:45:05 -0600

    modified docs page

--- integration_tests/generate_docs.sh
+++ integration_tests/generate_docs.sh
@@ -1,6 +1,6 @@
 dbt docs generate
 
-sed -i  s"|<title>dbt Docs</title>|<title>dbt-testgen Docs</title><link rel='stylesheet' href='/styles.css' />|"g target/index.html
+sed -i  s"|<title>dbt Docs</title>|<title>dbt-testgen Docs</title><link rel='stylesheet' href='/dbt-testgen/styles.css' />|"g target/index.html
 
 mkdir -p ../docs
 cp target/catalog.json ../docs
